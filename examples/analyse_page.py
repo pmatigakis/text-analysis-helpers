b559a5bf8e96bb32ad18bb3d61798b6a14842fd0
Panagiotis Matigakis <pmatigakis@gmail.com>
2018-08-01 23:15:27 +0300

    The example script will save the analysis result to an html file

--- examples/analyse_page.py
+++ examples/analyse_page.py
@@ -1,17 +1,34 @@
-import sys
+from argparse import ArgumentParser
+from os import getcwd
 
 import requests
+from jinja2.loaders import FileSystemLoader
+from jinja2 import Environment
 
 from text_analysis_helpers.html import HtmlAnalyser
 
 
+def get_arguments():
+    parser = ArgumentParser()
+    parser.add_argument("--output", default="analysis_result.html")
+    parser.add_argument("url")
+
+    return parser.parse_args()
+
+
 def main():
-    response = requests.get(sys.argv[1])
+    args = get_arguments()
 
+    response = requests.get(args.url)
     analyser = HtmlAnalyser()
-    result = analyser.analyse(response.text)
+    analysis_result = analyser.analyse(response.text)
 
-    print(result)
+    env = Environment(loader=FileSystemLoader(getcwd()))
+    template = env.get_template("analysis_result_template.html")
+    content = template.render(analysis_result=analysis_result)
+
+    with open(args.output, "w") as f:
+        f.write(content)
 
 
 if __name__ == "__main__":
