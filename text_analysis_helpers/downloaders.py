ef86f9ea56cb424a5f53c0aefff0773011ac1542
Panagiotis Matigakis <pmatigakis@gmail.com>
2018-10-23 21:20:33 +0300

    Added the exception that is reised if we were not able to download a web page

--- text_analysis_helpers/downloaders.py
+++ text_analysis_helpers/downloaders.py
@@ -1,18 +1,23 @@
 import requests
 
+from text_analysis_helpers.exceptions import WebPageDownloadError
 from text_analysis_helpers.models import WebPage
 
 
-def download_web_page(url, timeout=5, headers=None, verify=True):
+def download_web_page(url, timeout=5, **kwargs):
     response = requests.get(
         url,
         timeout=timeout,
-        headers=headers,
-        verify=verify
+        **kwargs
     )
 
     if response.status_code < 200 or response.status_code >= 300:
-        raise ValueError("invalid response status code")
+        raise WebPageDownloadError(
+            message="failed to download web page",
+            url=url,
+            status_code=response.status_code,
+            response=response.text
+        )
 
     return WebPage(
         url=url,
--- text_analysis_helpers/exceptions.py
+++ text_analysis_helpers/exceptions.py
@@ -1,2 +1,14 @@
 class TextAnalysisHelpersException(Exception):
     pass
+
+
+class WebPageDownloadError(TextAnalysisHelpersException):
+    def __init__(self, message=None, url=None, status_code=None,
+                 response=None):
+        super(WebPageDownloadError, self).__init__(
+            message, url, status_code, response)
+
+        self.message = message
+        self.url = url
+        self.status_code = status_code
+        self.response = response
